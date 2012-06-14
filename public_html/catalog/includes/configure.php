7aad0f6305cc955e10b983605e846413a4c2c18a
Jason <jason@ezimerchant.com>
2012-06-14 23:36:16 +1000

    first commit

---
+++ public_html/catalog/includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://osc3.ezimobile.biz');
+  define('HTTPS_SERVER', 'http://osc3.ezimobile.biz');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/catalog/');
+  define('HTTPS_COOKIE_PATH', '/catalog/');
+  define('DIR_WS_HTTP_CATALOG', '/catalog/');
+  define('DIR_WS_HTTPS_CATALOG', '/catalog/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/home/ezimobile/domains/osc3.ezimobile.biz/public_html/catalog/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'ezimobile');
+  define('DB_SERVER_PASSWORD', 'lU2M9dPdU4cKuzy0');
+  define('DB_DATABASE', 'osc3');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+?>
